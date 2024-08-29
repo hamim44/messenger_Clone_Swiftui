6bca2d6bcadd0b855a6483f56c894fb23c989128
Abrar hamim <abrarhamim4044@gmail.com>
2024-08-30 00:17:25 +0600

    Initial Commit

---
+++ Messenger_Clone/Messenger_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Messenger_CloneApp.swift
+//  Messenger_Clone
+//
+//  Created by Abrar Hamim on 30/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Messenger_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
