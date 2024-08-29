19577b5413e87a803f92b3519606e3d349a88e92
Abrar hamim <abrarhamim4044@gmail.com>
2024-08-30 00:36:55 +0600

    init commit

---
+++ Messenger_Clone/Core/Authentication/View/LogInView.swift
@@ -0,0 +1,18 @@
+//
+//  LogInView.swift
+//  Messenger_Clone
+//
+//  Created by Abrar Hamim on 30/8/24.
+//
+
+import SwiftUI
+
+struct LogInView: View {
+    var body: some View {
+        Text(" Log in")
+    }
+}
+
+#Preview {
+    LogInView()
+}
